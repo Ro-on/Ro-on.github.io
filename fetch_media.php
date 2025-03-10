a8e1d24329fa5b117778b8f7f4f09450b540157a
Valeria Zibrova <Vale.ro.on@yandex.ru>
2025-03-11 02:10:38 +0300

    Add new line

--- fetch_media.php
+++ fetch_media.php
@@ -1,9 +1,18 @@
 <?php
-$folder = $_GET['folder'];
-$directory = ($folder === 'images') ? 'images/' : (($folder === 'videos') ? 'videos/' : '');
+$allowedFolders = ['images', 'videos'];
+$folder = $_GET['folder'] ?? '';
+
+if (!in_array($folder, $allowedFolders)) {
+    // Если параметр не допустим
+    header('HTTP/1.1 400 Bad Request');
+    echo json_encode(array('error' => 'Неверный параметр folder.'));
+    exit;
+}
+
+$directory = $folder . '/';
 
 if (!is_dir($directory)) {
-    // Если директория не существует, возвращаем ошибку
+    // Если директория не существует
     header('HTTP/1.1 404 Not Found');
     echo json_encode(array('error' => 'Директория не найдена.'));
     exit;
