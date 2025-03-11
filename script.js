e0f07f1924a08f75872966ca33995aed1a8af37e
Valeria Zibrova <Vale.ro.on@yandex.ru>
2025-03-11 03:31:30 +0300

    More categories, more pictures

--- script.js
+++ script.js
@@ -3,23 +3,27 @@ const GITHUB_API_URL = "https://api.github.com/repos/Ro-on/ro-on.github.io/conte
 function openModal(img) {
     const modal = document.getElementById("modal");
     const modalImg = document.getElementById("modalImage");
-    modal.style.display = "block";
+    modal.style.display = "flex";
     modalImg.src = img.src;
 }
 
 function closeModal() {
-    document.getElementById("modal").style.display = "none";
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
 }
 
 function openVideoModal(video) {
     const videoModal = document.getElementById("videoModal");
     const modalVideo = document.getElementById("modalVideo");
-    videoModal.style.display = "block";
-    modalVideo.src = video.src;
+    const modalSource = document.getElementById("modalVideoSource");
+    videoModal.style.display = "flex";
+    modalSource.src = video.src;
+    modalVideo.load();
 }
 
 function closeVideoModal() {
-    document.getElementById("videoModal").style.display = "none";
+    const videoModal = document.getElementById("videoModal");
+    videoModal.style.display = "none";
     const modalVideo = document.getElementById("modalVideo");
     modalVideo.src = ""; // Остановить видео
 }
@@ -35,20 +39,32 @@ async function loadCategory(category) {
         if (item.type === 'file') {
             const filePath = item.download_url;
 
-            if (category === 'images') {
-                const img = document.createElement("img");
-                img.src = filePath;
-                img.alt = "Мем";
-                img.className = "gallery-image";
-                img.onclick = () => openModal(img);
-                gallery.appendChild(img);
-            } else if (category === 'videos') {
-                const video = document.createElement("video");
-                video.controls = true;
-                video.src = filePath;
-                video.onclick = () => openVideoModal(video);
-                gallery.appendChild(video);
-            }
+            const img = document.createElement("img");
+            img.src = filePath;
+            img.alt = "Мем";
+            img.className = "gallery-image";
+            img.onclick = () => openModal(img);
+            gallery.appendChild(img);
+        }
+    });
+}
+
+async function loadVideos() {
+    const response = await fetch(GITHUB_API_URL + 'videos');
+    const data = await response.json();
+
+    const gallery = document.getElementById("gallery");
+    gallery.innerHTML = ''; // Очистить текущий контент
+
+    data.forEach(item => {
+        if (item.type === 'file') {
+            const filePath = item.download_url;
+
+            const video = document.createElement("video");
+            video.controls = true;
+            video.src = filePath;
+            video.onclick = () => openVideoModal(video);
+            gallery.appendChild(video);
         }
     });
 }
